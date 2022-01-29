34c4733d85a9f9c9fe68554e0cfebedd9ab523e6
JJMaslen <44002981+JJMaslen@users.noreply.github.com>
2022-01-29 19:03:09 +0000

    turtle DONE

---
+++ ninturtlesUnity/Assets/Scripts/BasicCamera.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicCamera : MonoBehaviour
+{
+    float speedH;
+    float speedV;
+
+    float yaw;
+    float pitch;
+
+    public int speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speedH = 2.0f;
+        speedV = 2.0f;
+
+        yaw = 0.0f;
+        pitch = 0.0f;
+
+        speed = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FirstPersonCamera();
+        move();
+    }
+
+    void FirstPersonCamera()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        pitch = Mathf.Clamp(pitch, -90f, 90f);
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+
+    void move()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.position -= transform.up * speed * Time.deltaTime;
+        }
+    }
+
+}
